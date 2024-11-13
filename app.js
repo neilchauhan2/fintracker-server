087bd6d30b1601ca68c8d7f5b4891607fa88307e
Neil Chauhan <neilchauhan2@gmail.com>
2024-11-13 14:33:52 +0530

    Fix: Fix cors error on PROD

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ const transactionRoutes = require("./routes/transactionRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const allowedOrigins = [
+  "https://fintracker-client.vercel.app", // Production URL
+  "http://localhost:5173", // Development URL
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 // Test database connection and sync models
 sequelize
